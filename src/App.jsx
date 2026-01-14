e8bdd764d361549e4c483e6e436a7e4a0cb628c1
gaspym97 <angelgasparmiralles@gmail.com>
2026-01-14 12:19:02 -0500

    finishing set up + basic layout (first steps)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
-import './App.css'
+import { Search } from 'lucide-react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="p-6 text-2xl font-bold">Tailwind is working</div>
+      <div className="min-h-screen bg-[#1f232d] flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center gap-4 mb-80">
+          <h1 className="text-white text-5xl font-extrabold antialiased">Find a Pokemon</h1>
+          <button
+            type="button"
+            aria-label="Search"
+            className="mt-4 inline-flex size-20 items-center justify-center rounded-full border border-white/20 bg-white/10 text-white shadow-lg shadow-black/20 transition hover:bg-white/20"
+          >
+            <Search className="size-10" />
+          </button>
+        </div>
+      </div>
     </>
   )
 }
